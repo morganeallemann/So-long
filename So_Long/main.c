62af7d49d5177cfc5eca1b3f8e47036900f7dcd1
morganeallemann <116814173+morganeallemann@users.noreply.github.com>
2023-02-15 16:43:25 +0100

    Add files via upload

--- So_Long/main.c
+++ So_Long/main.c
@@ -6,11 +6,13 @@
 /*   By: malleman <malleman@42lausanne.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:37:09 by malleman          #+#    #+#             */
-/*   Updated: 2023/02/08 15:48:37 by malleman         ###   ########.fr       */
+/*   Updated: 2023/02/15 16:39:10 by malleman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 #include "so_long.h"
+#include<unistd.h>
+#include<stdio.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -20,27 +22,18 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
 int	main(void)
 {
 	void	*mlx;
+	void	*img;
 	void	*mlx_win;
-	t_data	img;
+	char	*relative_path = "./Player.xpm";
+	int		img_width = 32;
+	int		img_height = 32;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 30, 30, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
 	mlx_loop(mlx);
 }
-
