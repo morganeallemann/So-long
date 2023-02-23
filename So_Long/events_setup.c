310879cec8d8609d4ff157ea440c97a0678c1c8f
morganeallemann <116814173+morganeallemann@users.noreply.github.com>
2023-02-23 17:33:03 +0100

    Add files via upload

--- So_Long/events_setup.c
+++ So_Long/events_setup.c
@@ -15,7 +15,6 @@
 /* Fonction permettant de gerer les evenements du clavier. */
 int event_key(int keycode, t_game_params *game)
 {
-
     if (keycode == KEY_ESC)
     {
         mlx_destroy_window(game->mlx_ptr, game->win_ptr);
@@ -26,7 +25,6 @@ int event_key(int keycode, t_game_params *game)
         mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img.floor, game->pos_x * IMG_SIZE, game->pos_y *IMG_SIZE);
         game->pos_y--;
         mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img.player, game->pos_x * IMG_SIZE, game->pos_y *IMG_SIZE);
-        printf("pos_y: %d\n", game->pos_y);
     }
     else if (keycode == KEY_S && game->map.map[game->pos_y + 1][game->pos_x] != '1')
     {
